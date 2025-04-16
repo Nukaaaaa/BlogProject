d8bd7a4bb88f93f9ef1a3d2f9bcca5ab9be62d4d
Nuka <2017kazah@gmail.com>
2025-04-16 17:18:29 +0500

    second commit

--- main.go
+++ main.go
@@ -16,24 +16,19 @@ func main() {
 	r := gin.Default()
 
 	// Маршруты для работы с постами (Post)
-	r.GET("/posts", handlers.GetPosts)                          // Получить все посты
-	r.POST("/posts", handlers.AddPost)                          // Добавить новый пост
-	r.GET("/posts/:id", handlers.GetPostByID)                   // Получить пост по ID
-	r.PUT("/posts/:id", handlers.UpdatePost)                    // Обновить пост по ID
-	r.DELETE("/posts/:id", handlers.DeletePost)                 // Удалить пост по ID
-	r.GET("/posts/user/:id", handlers.GetPostsByUserID)         // Получить все посты по ID клиенты
-	r.GET("/posts/category/:id", handlers.GetPostsByCategoryID) // Получить все посты по ID категории
+	r.GET("/posts", middleware.AuthMiddleware(), handlers.GetPosts)                          // Получить все посты
+	r.POST("/posts", middleware.AuthMiddleware(), handlers.AddPost)                          // Добавить новый пост
+	r.GET("/posts/:id", middleware.AuthMiddleware(), handlers.GetPostByID)                   // Получить пост по ID
+	r.PUT("/posts/:id", middleware.AuthMiddleware(), handlers.UpdatePost)                    // Обновить пост по ID
+	r.DELETE("/posts/:id", middleware.AuthMiddleware(), handlers.DeletePost)                 // Удалить пост по ID
+	r.GET("/posts/user/:id", middleware.AuthMiddleware(), handlers.GetPostsByUserID)         // Получить все посты по ID клиенты
+	r.GET("/posts/category/:id", middleware.AuthMiddleware(), handlers.GetPostsByCategoryID) // Получить все посты по ID категории
 
 	// Маршруты для работы с категориями (Category)
-	r.GET("/categories", handlers.GetCategories) // Получить все категории
-	r.POST("/categories", handlers.AddCategory)  // Добавить новую категорию
-	auth := r.Group("/")
+	r.GET("/categories", middleware.AuthMiddleware(), handlers.GetCategories) // Получить все категории
+	r.POST("/categories", middleware.AuthMiddleware(), handlers.AddCategory)  // Добавить новую категорию
 
-	auth.Use(middleware.AuthMiddleware()) // защищённые маршруты
-	{
-		auth.GET("/users", handlers.GetUsers)
-		auth.POST("/users", handlers.AddUser)
-	}
+	r.GET("/users", middleware.AuthMiddleware(), handlers.GetUsers) // Получить всех пользователей
 	// Маршрут регистрации и входа
 	r.POST("/register", handlers.Register)
 	r.POST("/login", handlers.Login)
--- models/User.go
+++ models/User.go
@@ -4,5 +4,5 @@ type User struct {
 	ID       uint   `json:"id" gorm:"primaryKey"`
 	Name     string `json:"name"`
 	Email    string `json:"email" gorm:"unique"`
-	Password string `json:"password"`
+	Password string `json:"-"`
 }
